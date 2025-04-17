2f31fd3ff9bfe3ed9af7bca511630c4da4b7b1fe
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2025-04-17 02:19:17 -0300

    Choose the first word and go to the next when it's typed + space

--- src/script.js
+++ src/script.js
@@ -1,18 +1,41 @@
 // Aidan Carey, April 2025
 
 const currentWordsDOM = document.getElementById("current-words");
+const currentWordDOM = document.getElementById("current-word");
+const typingAreaDOM = document.getElementById("typing-area");
 
 // Choose a random word from the word list
 function randomWord() {
   return wordsList[Math.floor(Math.random() * wordsList.length)]
 }
 
-// Current words left to type
-currentWords = [];
-
-// Fill the current words list
+// Current words to type
+let currentWords = [];
 for (let i = 0; i < 20; i++) {
   currentWords.push(randomWord());
 }
 
-currentWordsDOM.textContent = currentWords;
+currentWordsDOM.textContent = currentWords.join(" ");
+
+// Current word to type
+let currentWordIndex = 0;
+let currentWord = currentWords[currentWordIndex];
+
+currentWordDOM.textContent = currentWord;
+
+typingAreaDOM.oninput = () => {
+  // Currently typed word
+  let typed = typingAreaDOM.value;
+
+  // Next word on space
+  if (typed === currentWord + " ") {
+    // Update current word
+    currentWordIndex++;
+    currentWord = currentWords[currentWordIndex];
+
+    currentWordDOM.textContent = currentWord;
+
+    // Clear typing area
+    typingAreaDOM.value = "";    
+  }
+}
